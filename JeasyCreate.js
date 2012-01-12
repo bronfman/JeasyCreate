d1765b1670e5e901ccecaf731ec69b73178f4e4f
Tingan Ho <tingan@clockies.com>
2012-01-12 23:49:31 +0100

    Updates made

--- JeasyCreate.js
+++ JeasyCreate.js
@@ -13,7 +13,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// Contact Tingan Ho tingan@clockies.com
+// Author: Tingan Ho <tingan@clockies.com>
 
 function append(appendElement, element){
 	var isInstance = appendElement instanceof jQuery;
